fff21ccf76e318ad49ea22f538f0e8dce19fb949
damian08k <damiankierzkowski08@wp.pl>
2021-08-27 18:42:36 +0200

    Create basic app routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { Box } from "@material-ui/core";
 
+import Error from "./components/Error/Error";
 import Posts from "./components/Posts/Posts";
 
 const queryClient = new QueryClient({
@@ -21,7 +24,19 @@ const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
             <Box width="100%" display="flex" justifyContent="center">
-                <Posts />
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <Redirect to="/posts" />
+                        </Route>
+                        <Route exact path="/posts">
+                            <Posts />
+                        </Route>
+                        <Route path="*">
+                            <Error message="Page not found." />
+                        </Route>
+                    </Switch>
+                </Router>
             </Box>
         </QueryClientProvider>
     );
